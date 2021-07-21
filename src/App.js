7b557746d6988698dd46775bd8dd0e92616cf3d9
leech-oeo <fairprice81000@gmail.com>
2021-07-22 03:13:17 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
 import Header from "./componnents/Header";
-import Tasks from "./componnents/Tasks";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddTask from "./componnents/AddTask";
+import {db} from "./Firebase"
+import { ListItem, ListItemText, Button } from "@material-ui/core";
+import { googleProvider } from "./authMethod";
+import SocialAuth from "./auth";
+
+
+
+
 function App() {
+  const [dbTask, setdbTask] = useState([])
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTask] = useState([]
-  )
+  const [tasks, setTask] = useState([])
+  useEffect(() => {
+    getData()
+  }, [])
+  function getData(){
+    db.collection("task").onSnapshot(function (querySnapshot){
+      setdbTask(
+        querySnapshot.docs.map((doc) => ({
+          id : doc.id,
+          text: doc.data().task,
+          day: doc.data().time,
+          reminder: doc.data().reminder
+        }))
+      )
+    })
+  }
   const deleteTask = (id) =>{
-    setTask(tasks.filter((task) => task.id !== id))
+    db.collection("task").doc(id).delete()
+
   }
-  const toggleReminder = (id) => {
-    setTask(tasks.map((task) => task.id ===id ? { ...task, reminder: !task.reminder} : task))
-  }
+  // const toggleReminder = (id) => {
+  //   setTask(tasks.map((task) => task.id ===id ? ({ ...task, reminder: !task.reminder}): task))
+  // }
   const addTask = (task)=>{
     const id = Math.floor(Math.random()*10000) +1
     const newTask = {id,...task}
     setTask([...tasks, newTask])
+    db.collection("task").add({
+      reminder: newTask.reminder,
+      task: newTask.text,
+      time: newTask.day,
+
+    })
   }
-  return (
+  const handleOnclick = async(provider)=>{
+    const res = await SocialAuth(provider)
+    console.log(res)
+  }
+   return (
+     
     <div className="container">
+      <Button onClick={()=>handleOnclick(googleProvider)} variant="contained" color="primary" size="small"git>
+      login
+      </Button>
       <Header name='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd= {showAddTask}/>
       {showAddTask &&<AddTask onAdd={addTask}/>}
-      {tasks.length >0 ?(<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder} />) : ('No Task')}
+      {/* {tasks.length >0 ?(<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder} />) : ('No Task')} */}
+      {dbTask.map(dbtask =>(
+        <div style={{display: "flex"}}>
+          <ListItem>
+            <ListItemText primary={dbtask.text} secondary={(dbtask.day)+(dbtask.reminder ? ("🔔") : ("🔕"))}  />
+          </ListItem>
+          <Button onClick={() => deleteTask(dbtask.id)}>X</Button>
+        </div>
+      ))}
+      
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/componnents/AddTask.js
+++ src/componnents/AddTask.js
@@ -1,9 +1,20 @@
 import {useState} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
 const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
-
+    
     const onSubmit = (e) => {
         e.preventDefault()
         if(!text){
@@ -13,25 +24,40 @@ const AddTask = ({ onAdd }) => {
 
 
         onAdd({text, day, reminder})
+        
         setDay('')
         setText('')
         setReminder(false)
     }
     return (
         <form className='add-form' onSubmit={onSubmit}>
-            <div className='form-control'>
+            {/* <div >
                 <label>Task</label>
                 <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)}/>
-            </div>
+            </div> */}
+            
+            <TextField id="standard-basic" label="Add Task" value={text} onChange={(e) => setText(e.target.value)}/>
             <div className='form-control'>
                 <label>Day & Time</label>
                 <input type='datetime-local' value={day} onChange={(e) => setDay(e.target.value)}/>
             </div>
+
+            
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' checked = {reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+                <Checkbox checked = {reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked) }/>
+                {/* <input type='checkbox' checked = {reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/> */}
             </div>
-            <input type='submit' value='Save Task' className='btn' />
+            {/* <input type='submit' value='Save Task'/> */}
+            <Button
+                value='Save Task'
+                variant="outlined"
+                color="primary"
+                size="large"
+                type='submit'
+                className={useStyles().button}
+                startIcon={<SaveIcon />}
+            >Save Task</Button>
         </form>
     )
 }
